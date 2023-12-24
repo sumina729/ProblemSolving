f554c09c8d16c0576573048a73d736b7eddae6a7
Sumin Lee <114402652+sumina729@users.noreply.github.com>
2023-12-24 23:12:50 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include  <math.h>
+#include <string.h>
+
+int main(void){
+
+    double A, B;
+    scanf("%lf", &A);
+    scanf("%lf", &B);
+    
+    printf("%.9lf", A/B);
+
+    return 0;
+}
