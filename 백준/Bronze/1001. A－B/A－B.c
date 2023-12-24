8db14d0ad4aca6aeb9d8fa082482ae5326c411f2
Sumin Lee <114402652+sumina729@users.noreply.github.com>
2023-12-24 23:08:38 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include  <math.h>
+#include <string.h>
+
+int main(void){
+
+    int A, B;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    printf("%d", A-B);
+
+    return 0;
+}
