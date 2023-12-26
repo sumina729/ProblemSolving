94f5357a3540038440c9995da9f4cbf720c15e1b
Sumin Lee <114402652+sumina729@users.noreply.github.com>
2023-12-26 23:42:21 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include  <math.h>
+#include <string.h>
+
+int main(void){
+
+    int N;
+    int b,s;
+    
+    scanf("%d", &N);
+    b = 0;
+    s = 1+(N-1)*2;
+    
+
+    for(int i=0; i<N; i++){
+        for(int j=0; j<b; j++){
+            printf(" ");
+        }
+        for(int j=0; j<s; j++){
+            printf("*");
+        }
+        printf("\n");
+        b++;
+        s-=2;
+    }
+
+    return 0;
+}
