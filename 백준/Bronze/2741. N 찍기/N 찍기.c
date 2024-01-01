c0d7c7f5c4880972ace2a4ef6d330f2cfb222f4f
Sumin Lee <114402652+sumina729@users.noreply.github.com>
2024-01-01 23:19:08 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include  <math.h>
+#include <string.h>
+
+int main(void){
+
+    int N;
+
+    scanf("%d", &N);
+
+    for(int i=1; i<=N; i++){
+        printf("%d\n", i);
+    }
+    
+
+
+    return 0;
+}
