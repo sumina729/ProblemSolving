baadfa2306e234489424287d7e0cfdeeade2ebf8
Sumin Lee <114402652+sumina729@users.noreply.github.com>
2024-08-07 19:12:07 +0900

    [Silver III] Title: N과 M (3), Time: 2452 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,20 @@
+N, M = map(int, input().split())
+sequence = [0 for i in range(M)]
+
+def backtracking(m):
+    if m == M:
+        for i in range(M):
+            print(sequence[i], end=" ")
+        print()
+        return
+    
+    for i in range(1, N+1):
+        is_tmp = True
+        # for j in range(m):
+        #     if(i == sequence[j]): is_tmp = False
+        if(is_tmp) :
+            sequence[m] = i
+            backtracking(m+1)
+        
+                 
+backtracking(0)  
