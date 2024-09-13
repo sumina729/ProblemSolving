b6b5a8de77c0049ba1628fdfeb9b5654cf13c8ab
Sumin Lee <114402652+sumina729@users.noreply.github.com>
2024-09-13 16:48:17 +0900

    [Silver III] Title: 소수 구하기, Time: 564 ms, Memory: 41492 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 a, b= map(int, input().split())
 array = [True for _ in range(b+1)]
 
-for i in range(1, b+1):	# 2부터 x의 제곱근까지의 숫자
+for i in range(1, int(math.sqrt(b) + 1)):	# 2부터 x의 제곱근까지의 숫자
     if i ==1 :
         array[i] = False
 
