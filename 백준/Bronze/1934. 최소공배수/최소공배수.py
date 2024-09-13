da4c48d92068784d30978796119bb9950ab2d727
Sumin Lee <114402652+sumina729@users.noreply.github.com>
2024-09-13 16:15:40 +0900

    [Bronze I] Title: 최소공배수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+#최대 공약수
+def gcd(a, b):
+    if b ==0:
+        return a
+    if a%b == 0:
+        return b
+    else :
+        return gcd(b, a%b)
+
+n = int(input())
+
+for i in range(n):  
+    a, b = map(int, input().split())
+    # print(gcd(a, b))
+    print(int(a*b/gcd(a, b)))
