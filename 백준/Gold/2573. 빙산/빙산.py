e7e9ac5674420b0dfe9cc74bf837387f2d6c9150
Sumin Lee <114402652+sumina729@users.noreply.github.com>
2025-03-25 23:38:52 +0900

    [Gold IV] Title: 빙산, Time: 2260 ms, Memory: 35812 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,77 +1,60 @@
+import sys
 from collections import deque
-N, M = map(int, input().split())
+input = sys.stdin.readline
 
-graph = [list(map(int, input().split()))for _ in range(N)]
-visit = [[0 for _ in range(M)] for _ in range(N)]
-
-
-# print(graph)
-
-# 인접해있는 칸 수 확인하기
-# 인접해있는 칸 수 만큼 녹이기
-# 덩어리가 몇개인지 확인하기(BFS)
-
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
-sea = [[0 for _ in range(M)] for _ in range(N)]
-
-def melt(x, y):
-    for i in range(4):
-        nx = x+dx[i]
-        ny = y+dy[i]
-
-        if -1 < nx < M and -1 < ny < N and graph[ny][nx] == 0:
-            sea[y][x]+=1
 
 def bfs(x, y):
-    que = deque()
-    que.append((x, y))
-    visit[y][x] = 1
+    q = deque([(x, y)])
+    visited[x][y] = 1
+    seaList = []
 
-    while que:
-        x, y = que.popleft()
-        
+    while q:
+        x, y = q.popleft()
+        sea = 0
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if not graph[nx][ny]:
+                    sea += 1
+                elif graph[nx][ny] and not visited[nx][ny]:
+                    q.append((nx, ny))
+                    visited[nx][ny] = 1
+        if sea > 0:
+            seaList.append((x, y, sea))
+    for x, y, sea in seaList:
+        graph[x][y] = max(0, graph[x][y] - sea)
 
-            if -1 < nx < M and -1 < ny < N and visit[ny][nx] == 0 and graph[ny][nx] > 0:
-                visit[ny][nx] = 1
-                que.append((nx, ny))
+    return 1
 
 
-ans = 0
-while True:
-    visit = [[0 for _ in range(M)] for _ in range(N)]
-    cnt = 0
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-    for y in range(N):
-        for x in range(M):
-            if graph[y][x] > 0:
-                cnt+=1
-                melt(x, y)
+ice = []
+for i in range(n):
+    for j in range(m):
+        if graph[i][j]:
+            ice.append((i, j))
 
-    if cnt ==  0:
-        print(0)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+year = 0
+
+while ice:
+    visited = [[0] * m for _ in range(n)]
+    delList = []
+    group = 0
+    for i, j in ice:
+        if graph[i][j] and not visited[i][j]:
+            group += bfs(i, j)
+        if graph[i][j] == 0:
+            delList.append((i, j))
+    if group > 1:
+        print(year)
         break
+    ice = sorted(list(set(ice) - set(delList)))
+    year += 1
 
-    for y in range(N):
-        for x in range(M):
-            graph[y][x] = max(0, graph[y][x]-sea[y][x])
-            sea[y][x] = 0
-
-    
-
-    grupe_n = 0
-    for y in range(N):
-        for x in range(M):
-            if graph[y][x] > 0 and visit[y][x] == 0:
-                grupe_n+=1
-                bfs(x, y)
-    
-    ans+=1
-    if grupe_n > 1:
-        print(ans)
-        break
-    
+if group < 2:
+    print(0)
