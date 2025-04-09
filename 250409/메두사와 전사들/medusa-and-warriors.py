71aa345a2df907a914bcbd635287199fb0b274c5
Sumin Lee <114402652+sumina729@users.noreply.github.com>
2025-04-09 23:29:27 +0900

    Title: 메두사와 전사들, Time: 719ms, Memory: 25MB, Status: Passed - Codetree

--- 250409/메두사와 전사들/medusa-and-warriors.py
+++ 250409/메두사와 전사들/medusa-and-warriors.py
@@ -9,8 +9,6 @@ def bfs(x, y):
     que = deque()
     que.append((x, y))
 
-
-    # tmp_pan = [[[] for _ in range(N)] for _ in range(N)]
     tmp_pan = [[None for _ in range(N)] for _ in range(N)]
     visit[y][x] = 1
 
@@ -220,9 +218,6 @@ def see(mx, my, d):
 
         n+=1
 
-    # print("시선", d, "개수", cnt)
-    # for y in see_pan:
-    #     print(y)
     return cnt, junsa_dol, see_pan
 
 def see_all(x, y):
@@ -238,23 +233,16 @@ def see_all(x, y):
             junsa_dol = tmp1[:]
             see_pan = tmp2[:]
 
-
-    # print(max_d, max_cnt, junsa_dol)
     return max_cnt, junsa_dol, see_pan
-    #가장 많이 돌 되는거
 
 
 def move_junsa(x, y, see_sun):
     global mx, my
-
-    # print("전사 이동 시작", x, y)
-
     move_n = 0
 
     min_x = x
     min_y = y
     min_d = abs(x-mx)+abs(y-my)
-
 
     for i in [0, 1, 2, 3]:
         nx = x + dx[i]
@@ -287,10 +275,8 @@ def move_junsa(x, y, see_sun):
 
 
     if min_x == mx and min_y == my:
-        # print("전사 이동:", [min_x, min_y], "움직임수:", move_n, "메두사위치: ", mx, my, "메두사 공격여뷰", 0, "메두사에서 거리", min_d)
         return 1,  move_n, [min_x, min_y]
     else:
-        # print("전사 이동:", [min_x, min_y], "움직임수:", move_n, "메두사위치: ", mx, my, "메두사 공격여뷰", 1, "메두사에서 거리", min_d)
         return 0, move_n, [min_x,min_y]
 
 
@@ -301,11 +287,9 @@ def move_All_junsa(dol, see_sun):
     move_n = 0
     atec_n = 0
 
-    # print("최종 시선", )
     global m_list
     for x, y in m_list:
         if not [x, y] in dol:
-            # print("이동할 전사:", x, y)
             a, n, tmpl = move_junsa(x, y,see_sun)
 
             move_n+=n
@@ -313,14 +297,10 @@ def move_All_junsa(dol, see_sun):
 
             if a==0:
                 new_m_list.append(tmpl)
-                # print("전사추가", new_m_list)
 
         else:
-            # print("돌되서 못움직인 전사 전사:", x, y)
             new_m_list.append([x,y])
-            # print("전사추가", new_m_list)
 
-    # print("최종전사", new_m_list)
     return new_m_list, move_n, atec_n
 
 
@@ -331,7 +311,6 @@ tmp = list(map(int, input().split()))
 
 m_list = []
 for i in range(M):
-    # m_list.append(tmp[i*2: i*2+2]) #y, x 순
     m_list.append([tmp[i*2+1], tmp[i*2]])
 
 pan = [list(map(int, input().split())) for _ in range(N)]
@@ -341,27 +320,13 @@ m_route = []
 m_route_n = N*N
 dol = [[]]
 
-#입력확인
-# print(N, M)
-# print(my, mx, ey, ex)
-# print(m_list)
-# for y in pan:
-#     print(y)
-
 # 경로 찾기
 path = bfs(mx, my)
 if path == -1:
     print("-1")
 else:
 
-    # print("공원가는 경로")
-    # print(len(path), path)
-
     for mx, my in path:
-
-        # print()
-        # print()
-        # print("턴 시작", mx, my)
 
         if mx == ex and my == ey:
             print(0)
@@ -376,41 +341,15 @@ else:
             if not (mx==ax and my ==ay):
                 nuw_m.append([ax,ay])
         m_list = nuw_m[:]
-        # print("메두사 욺직이고 최종 전사")
-        # print(m_list)
-
 
         #메두사 회전 하며서 돌로 만들수 있는 거 확인
         dol_n, dol, see_sun = see_all(mx, my)
-        # print("돌된 매두사 개수랑 배열: ",dol_n, dol)
         ans_dol+=dol_n
 
-        # print("메두사와 전사 위치")
-        # for y in range(N):
-        #     for x in range(N):
-        #         if y == my and x == mx:
-        #             print(3, end=" ")
-        #         elif [x, y] in m_list:
-        #             print(2, end=" ")
-        #         elif see_sun[y][x] == 1:
-        #             print(1, end=" ")
-        #         else:
-        #             print(0, end=" ")
-        #     print()
-
         #전시 욺직이기,
-        # print('총전사', m_list)
         m_list, m, a = move_All_junsa(dol, see_sun) #new_m_list, move_n, atec_n
-        # print("최최종 저사", m_list)
         ans_move+=m
         ans_atec+=a
 
-        # print("================답")
-        # print("욺직인 전사수:", ans_move)
-        # print("돌이 된 전사수:", ans_dol)
-        # print("메두사를 공격한 전사수:", ans_atec)
-        # print("================")
         print(ans_move, ans_dol, ans_atec)
-        # break
 
-    # dol_n, dol = see_all(2, 0) # 될된 수, 돌외 있는 전사 위치
