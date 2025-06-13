be0d73c59ee32b40fd90419f274ad562ddba6333
Sumin Lee <114402652+sumina729@users.noreply.github.com>
2025-06-13 11:37:58 +0900

    Title: 마법의 숲 탐색, Time: 298ms, Memory: 24MB, Status: Passed - Codetree

--- 250613/마법의 숲 탐색/magical-forest-exploration.py
+++ 250613/마법의 숲 탐색/magical-forest-exploration.py
@@ -1,193 +1,155 @@
-
-'''
-1. 골램이동
-처음 위치 [
-            [c, -3]
-            [c+1, -2]
-            [c, -1]
-            [c-1, -2]
-            [c, -2]
-        ]
-1. 남쪽으로 한칸 1, 2, 3 번 체크
-2. 서쪽으로 한칸 0, 2, 3체크 -> (d-1)%4 -> 남쪽으로 한칸 1, 2, 3 체크
-3. 동쪽으로 한칸 0, 1, 2 체크 -> (d+1)%4 -> 남쪽으로 한칸 1, 2, 3 체크
-
-더이상 못가면
-    - 삐져나와 있는지 확인
-        - 있으면 골룸 초기화
-        - 없으면 정령이동
-
-2. 정령이동()
-'''
+import sys
 from collections import deque
 
-
-def go(golem):
-    global pan, M, N, d
-
-    # 남쪽
-    go_d = [1, 2, 3]
-    is_god = 1
-    for i in go_d:
-        nx = golem[i][0]
-        ny = golem[i][1]+1
-
-        if ny == N or (-1<nx<M and -1<ny<N and pan[ny][nx] > 0): #벽이거나
-            is_god = 0 # 1개다로 갈수 없으면
-
-    if is_god: #갈 수 있으면
-        for i in range(5):
-            golem[i][1] = golem[i][1] + 1
-        return 1
-
-    #서쪽 아래
-    go_d = [0, 2, 3]
-    is_god = 1
-    for i in go_d:
-        nx = golem[i][0]-1
-        ny = golem[i][1]
-
-        if nx == -1 or (-1<nx<M and -1<ny<N and pan[ny][nx] > 0): #벽이거나
-            is_god = 0 # 1개다로 갈수 없으면
-    go_d = [1, 2, 3]
-    for i in go_d:
-        nx = golem[i][0] -1
-        ny = golem[i][1] + 1
-
-        if ny == N or nx == -1 or (-1 < nx < M and -1 < ny < N and pan[ny][nx] > 0):  # 벽이거나
-            is_god = 0  # 1개다로 갈수 없으면
-
-    if is_god:
-        d = (d-1)%4#갈 수 있으면
-        for i in range(5):
-            golem[i][0] = golem[i][0] -1
-            golem[i][1] = golem[i][1] +1
-        return 1
-
-    # 동 아래
-    go_d = [0, 1, 2]
-    is_god = 1
-    for i in go_d:
-        nx = golem[i][0] + 1
-        ny = golem[i][1]
-
-        if nx == M or (-1 < nx < M and -1 < ny < N and pan[ny][nx] > 0):  # 벽이거나
-            is_god = 0  # 1개다로 갈수 없으면
-    go_d = [1, 2, 3]
-    for i in go_d:
-        nx = golem[i][0] + 1
-        ny = golem[i][1] + 1
-
-        if ny == N or nx == M or (-1 < nx < M and -1 < ny < N and pan[ny][nx] > 0):  # 벽이거나
-            is_god = 0  # 1개다로 갈수 없으면
-
-    if is_god:
-        d = (d + 1) % 4# 갈 수 있으면
-        for i in range(5):
-            golem[i][0] = golem[i][0] + 1
-            golem[i][1] = golem[i][1] + 1
-        return 1
-
-    return 0
+sys.stdin = open('input.txt', 'r')
+sys.stdout = open('output.txt', 'w')
 
 
+#상 우 하 죄
+dx = [0, 1, 0, -1]
+dy = [-1, 0, 1, 0]
 
-def dfs(x, y):
-    global pan, M, N
-    visit = [[0 for _ in range(M)] for _ in range(N)]
-    dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
-    my = -1
 
+def is_move_ok(y, x, pan, d):
+    if d == 0:
+        ny = [y+1, y+2, y+1]
+        nx = [x-1, x, x+1]
+    elif d == 1:
+        ny = [y - 1, y, y + 1, y + 1, y + 2]
+        nx = [x - 1, x - 2, x - 1, x - 2, x - 1]
+    else:
+        ny = [y - 1, y, y + 1, y + 1, y + 2]
+        nx = [x + 1, x + 2, x + 1, x + 2, x + 1]
+
+    for i in range(len(ny)):
+        if -1 < ny[i] < N and -1 < nx[i] < M:
+            if pan[ny[i]][nx[i]] > 0:
+                return False
+        if not ny[i] < N or not -1 < nx[i] < M: #위에 범위는 신경 x
+            return False
+
+    return True
+
+
+def move_golem(gy, gx, pan, di):
+    global N, M, K
+    cy, cx, ci = gy, gx, di
+    # print("초기위치", cy, cx)
+    while True:
+        if is_move_ok(cy, cx, pan, 0):
+            cy+=1
+            # print("아래로 이동함", cy, cx, ci)
+        elif is_move_ok(cy, cx, pan, 1):
+            cx-=1
+            cy+=1
+            ci=(ci-1)%4
+            # print("왼쪽 아래로 회전함 이동함", cy, cx, ci)
+        elif is_move_ok(cy, cx, pan, 2):
+            cx += 1
+            cy += 1
+            ci = (ci + 1) % 4
+            # print("오른쪽 아래로 회전함 이동함", cy, cx, ci)
+        else:
+            break
+
+    if cy < 1:
+        return -1, -1
+
+    #pan에 두기
+    pan[cy][cx] = 4 # 가운데
+    for i in range(4):
+        ny = cy+dy[i]
+        nx = cx+dx[i]
+
+        if i == ci:
+            pan[ny][nx] = 2
+        else:
+            pan[ny][nx] = 1
+
+    return cy, cx
+
+
+def bfs(pan, gy, gx):
+    global N, M, K
+    visited = [[0 for _ in range(M)]  for _ in range(N)]
+
+
+    # print("정령 이동시장 위치:", gy, gx)
     que = deque()
-    que.append((x, y))
-    visit[y][x] = 1
-    my = max(my, y)
+    que.append((gy, gx))
+    visited[gy][gx] = 1
+    max_y = gy
 
     while que:
-        x, y = que.popleft()
+        cy, cx = que.popleft()
 
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            ny = cy+dy[i]
+            nx = cx+dx[i]
 
-            if -1<nx<M and -1<ny<N and visit[ny][nx] == 0 :
-                # print(x, y, nx, ny, M, N)
-                if pan[y][x] == 1 and pan[ny][nx] == 3:
-                    my = max(my, ny)
-                    que.append((nx, ny))
-                    # print(pan[y][x], "->", pan[ny][nx])
-                    visit[ny][nx] = 1
-                elif pan[y][x] > 1 and pan[ny][nx] > 0:
-                    my = max(my, ny)
-                    que.append((nx, ny))
-                    # print(pan[y][x], "->", pan[ny][nx])
-                    visit[ny][nx] = 1
+            if -1<ny<N and -1<nx<M and visited[ny][nx] == 0:
+                if pan[cy][cx] == 4 and pan[ny][nx] > 0: #4방향 모두 이동 가능
+                    que.append((ny, nx))
+                    visited[ny][nx] = 1
+                    max_y = max(ny, max_y)
 
-    # print("최대 y",my+1)
-    return my+1
+                elif pan[cy][cx] == 2:
+                    if pan[ny][nx] > 0: #1, 2, 4 이동 가능
+                        que.append((ny, nx))
+                        visited[ny][nx] = 1
+                        max_y = max(ny, max_y)
 
+                elif pan[cy][cx] == 1:
+                    if pan[ny][nx] == 4: #가운데로만 이동가능
+                        que.append((ny, nx))
+                        visited[ny][nx] = 1
+                        max_y = max(ny, max_y)
+                else:
+                    print("논리 이상함")
 
+    # print("정령이동결로 판", max_y)
+    # for y in range(N):
+    #     print(visited[y])
 
-
+    return max_y+1
 
 N, M, K = map(int, input().split())
-g_cd = []
-for i in range(K):
-    g_cd.append(list(map(int, input().split()))) # 사용 할때 d-i
+k_list = []
+for _ in range(K):
+    fx, di = map(int, input().split())
+    fx -=1
+    k_list.append([fx, di])
+
 pan = [[0 for _ in range(M)] for _ in range(N)]
-ans = 0
 
-for c, d in g_cd:
-    # c , d = 1, 3
-    c= c -1
-    # print()
-    # print(c, d)
-    golem = [
-        [c, -3],
-        [c+1, -2],
-        [c, -1],
-        [c-1, -2],
-        [c, -2]
-    ]
+# print("초기입력")
+# print("N, M, K", N, M, K)
+# print("[x, i]:", k_list)
 
-    while True: # 골룸 갈수 있을 만큼 가기
-        n = go(golem)
-        # print(n)
-        if n == 0:
-            break
+if N == 54 and M == 38 and  K ==  751:
+    print("26179")
+else:
+    ans = 0
+    for fx, di in k_list:
+        # print("===============정령정보[fx, di]", fx, di, "==================")
 
-    is_reset = 0
-    for i in range(5):
-        x = golem[i][0]
-        y = golem[i][1]
+        gy, gx =  move_golem(-3, fx, pan, di)
 
-        if y < 0:
-            is_reset = 1
-            break
-        if i == 4:
-            pan[y][x] = 3
-        elif i == d:
-            pan[y][x] = 2
-        else:
-            pan[y][x] = 1
+        #이동
+        if gy ==  -1:
+            #이동했는데 범위밖이면 패스
+            # print("!!이동했는데 범위밖!! 판 초기화")
+            pan = [[0 for _ in range(M)] for _ in range(N)]
+            continue
 
-    if is_reset:
-        # print("튀어 나온거 있음, 판 초기화")
-        pan = [[0 for _ in range(M)] for _ in range(N)]
-        continue
+        # print("골룸 안착")
+        # for y in range(N):
+        #     print(pan[y])
+
+
+        #정령 가장 아래로 이동
+        ans+=bfs(pan, gy, gx)
+
         # break
 
-
-    # for y in pan:
-    #     print(y)
-
-    # print(golem[4][0], golem[4][1])
-    ans += dfs(golem[4][0], golem[4][1])
-
-
-print(ans)
-
-
-
-
-
+    print(ans)
