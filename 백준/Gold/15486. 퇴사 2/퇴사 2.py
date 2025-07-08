b56f4c666d4af08361ae474d4e2ea2efd9becef6
Sumin Lee <114402652+sumina729@users.noreply.github.com>
2025-07-08 15:37:49 +0900

    [Gold V] Title: 퇴사 2, Time: 3200 ms, Memory: 348768 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -8,16 +8,15 @@ dp = [0 for _ in range(N+1)]
 max_v = 0
 
 for i in range(1, N+1):
-    t = c_list[i-1][0]
-    p = c_list[i-1][1]
+    get_day = i+c_list[i-1][0]-1
+    pay = c_list[i-1][1]
     
-    if i+t-1 <= N:
-        dp[i+t-1] = max(dp[i+t-1], max_v+p)
-    
-    max_v = max(dp[i], max_v) 
+    if get_day <= N:
+        n = max_v+pay
+        dp[get_day] = max(dp[get_day], n)
 
-    # print(dp, max_v)
+    max_v = max(dp[i], max_v) 
     
-   
+    # print(dp, max_v)
 
 print(max(dp)) 
