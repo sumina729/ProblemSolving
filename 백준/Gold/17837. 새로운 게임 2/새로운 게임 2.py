d7e852f89336b4c6d6340a48de0a467b6a8f62e1
Sumin Lee <114402652+sumina729@users.noreply.github.com>
2025-09-05 21:10:08 +0900

    [Gold II] Title: 새로운 게임 2, Time: 252 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.py
+++ 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.py
@@ -6,7 +6,6 @@ m_list = []
 
 dy = [0, 0, -1, 1]
 dx = [1, -1, 0, 0]
-
 re_d = [1, 0, 3, 2]
 
 for i in range(K):
@@ -15,24 +14,14 @@ for i in range(K):
     m_pan[y-1][x-1].append(i)
     m_list.append([y-1, x-1, d-1])
 
-# print(m_list)
-# print()
-# for m in m_pan:
-#     print(m)
-
-
 
 def get_move_list(my, mx, n):
-
-    # print(my, mx, n, m_pan[my][mx])
 
     for i in range(0, len(m_pan[my][mx])):
         if m_pan[my][mx][i] == n:
             split_i = i
-            # print(split_i)
             break
-    
-    
+      
     move_list = m_pan[my][mx][split_i:]
     m_pan[my][mx] = m_pan[my][mx][:split_i]
 
@@ -44,73 +33,43 @@ while True:
     if ans > 1000:
         print(-1)
         exit()
-    # print(ans, "턴 시작")
+
     for k in range(K):
         
-        # print("================시작", k)
         cy = m_list[k][0]
         cx = m_list[k][1]
         cd = m_list[k][2]
 
-
         move_list = get_move_list(cy, cx, k)
-        # print(move_list)
 
         ny = cy+dy[cd]
         nx = cx+dx[cd]
 
-        # print(ny, nx, cd)
 
-
-        #일차확인 처음에 이거버면 방향 바꾸기
-        if not (-1 < ny < N and  -1< nx < N):
-            # print("범위넘어감 방향 바꿈")
+        #1차확인
+        if not (-1 < ny < N and  -1< nx < N) or c_pan[ny][nx] == 2:
             cd = re_d[cd]
 
             ny = cy+dy[cd]
             nx = cx+dx[cd]
 
-        elif c_pan[ny][nx] == 2:
-            # print("파안색임 방향 바꿈")
-            cd = re_d[cd] 
 
-            ny = cy+dy[cd]
-            nx = cx+dx[cd]
-
-        # print(ny, nx, cd)
         #2차
         if not(-1 < ny < N and  -1< nx < N) or c_pan[ny][nx] == 2:
-            # print("또 못움직임")
-            m_pan[cy][cx] = m_pan[cy][cx]+move_list
-
-            for ni in move_list:
-                m_list[ni][0] = cy
-                m_list[ni][1] = cx
-            
-
-        elif c_pan[ny][nx] == 0: # 흰색이면
-            m_pan[ny][nx] = m_pan[ny][nx]+move_list
-
-            for ni in move_list:
-                m_list[ni][0] = ny
-                m_list[ni][1] = nx
+            ny = cy
+            nx = cx
 
         elif  c_pan[ny][nx] == 1: 
             move_list.reverse()
-            m_pan[ny][nx] = m_pan[ny][nx]+move_list
 
-            for ni in move_list:
-                m_list[ni][0] = ny
-                m_list[ni][1] = nx
+        m_pan[ny][nx] = m_pan[ny][nx]+move_list
+
+        for ni in move_list:
+            m_list[ni][0] = ny
+            m_list[ni][1] = nx
 
         m_list[k][2] = cd #방향 맞추기
 
-            
-        
-        # print()
-        # print(m_list)
-        # for m in m_pan:
-        #     print(m)
         
         for y in range(N):
             for x in range(N):
